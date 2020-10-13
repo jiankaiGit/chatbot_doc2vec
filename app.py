44c2b5448e83b1b17ae0c0603ca1673e2885d2d3
eason <jiankai801203@gmail.com>
2020-10-14 00:25:54 +0800

    update

--- app.py
+++ app.py
@@ -13,16 +13,14 @@ import gensim
 import os
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import linebotConfig
-
-from linebotConfig import getToken,getKey
 from questionList import selectedQuestion
 
 # Channel Access Token
 myToken = getToken()
 myKey = getKey()
-line_bot_api = LineBotApi(myToken)
+line_bot_api = LineBotApi("jqeFTerwBf33iLEXJNLYiB3Ub4wboThj5RtlMM4Ank2qMqwOga7yGrvtx/hByMdENKtVNJvD/fELbO8/UCeNCpsTzXrPBjOXqaVPXlSudGWET/JmQiB9ubxT2fyD9WBVB7fj7JCb4jHysu8QE1xMXgdB04t89/1O/w1cDnyilFU=")
 # Channel Secret
-handler = WebhookHandler(myKey)
+handler = WebhookHandler("994675102d50dfee59c7edfc417ecbc0")
 thankString = "謝謝您的參與"
 AnswererCurQuestIndex = {}
 labMappingTable = {'1':"實驗室一",'2':"實驗室二",'3':"實驗室三",'4':"實驗室四"}
